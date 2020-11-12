50cad225d6acf74d7805766231697ce71ac754d7
Aya Rida <aya.rida30@gmail.com>
2020-11-12 10:27:17 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  render(){
+    return (
+      <form id="to-do-form">
+        <input type="text" placeholder="Enter your task here.." />
+        <button type="submit">Add</button>
+      </form>
+    );
+
+  }
 }
 
 export default App;
